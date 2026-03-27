c906950b6276c1e73d9479c9900994854db95077
pamphoux <108572694+pamphoux@users.noreply.github.com>
2026-03-27 21:05:42 +0100

    Update createLogin.sql

--- Scripts/Maintenance/Install/createLogin.sql
+++ Scripts/Maintenance/Install/createLogin.sql
@@ -1,1 +1,18 @@
+-- Ouvrez un Terminal
+-- Methode 1
+sudo mariadb
+GRANT ALL PRIVILEGES ON *.* TO 'ext_dev'@'localhost' IDENTIFIED BY '43wkSQtCQYxFAc4d' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'admin2si'@'localhost' IDENTIFIED BY '2LkXvNH4kgyAii9N7nNi' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+QUIT
+-- Ou EXIT ;)
 
+-- Methode 2
+sudo mariadb
+CREATE USER 'ext_dev'@'localhost' IDENTIFIED BY '43wkSQtCQYxFAc4d';
+GRANT ALL ON *.* TO 'ext_dev'@'localhost' IDENTIFIED BY '43wkSQtCQYxFAc4d';
+CREATE USER 'admin2si'@'localhost' IDENTIFIED BY '2LkXvNH4kgyAii9N7nNi';
+GRANT ALL ON *.* TO 'admin2si'@'localhost' IDENTIFIED BY '2LkXvNH4kgyAii9N7nNi';
+FLUSH PRIVILEGES;
+QUIT
+-- Ou EXIT ;)
