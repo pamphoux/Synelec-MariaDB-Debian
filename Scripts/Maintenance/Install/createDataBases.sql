9fbe468204c8e21d4203d2b7e342f41e21f9e41b
pamphoux <108572694+pamphoux@users.noreply.github.com>
2026-03-27 21:31:22 +0100

    Update createDataBases.sql

--- Scripts/Maintenance/Install/createDataBases.sql
+++ Scripts/Maintenance/Install/createDataBases.sql
@@ -1,9 +1,11 @@
--- Database sidelc
+-- Database sidelc // !!! Doit être crée par la connexion MySQL admin2si !!!
 -- Ouvrez un Terminal
 mariadb -u admin2si -p2LkXvNH4kgyAii9N7nNi -h localhost -P 3306
 DROP SCHEMA IF EXISTS `sidelc`;
 CREATE DATABASE `sidelc` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
 
+-- Database extranet // !!! Doit être crée par la connexion MySQL ext_dev !!! 
+-- Ouvrez un Terminal
 mariadb -u ext_dev -p43wkSQtCQYxFAc4d -h localhost -P 3306
 DROP SCHEMA IF EXISTS `extranet`;
 CREATE DATABASE `extranet` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
