8bf3bd70ff924c1d98d4c61d2992be1374d0ce38
pamphoux <108572694+pamphoux@users.noreply.github.com>
2026-03-27 21:24:31 +0100

    Update createDataBases.sql

--- Scripts/Maintenance/Install/createDataBases.sql
+++ Scripts/Maintenance/Install/createDataBases.sql
@@ -1,3 +1,9 @@
 -- Database sidelc
 -- Ouvrez un Terminal
-mariadb -u admin2si -p2LkXvNH4kgyAii9N7nNi -h localhost -p 3306
+mariadb -u admin2si -p2LkXvNH4kgyAii9N7nNi -h localhost -P 3306
+DROP SCHEMA IF EXISTS `sidelc`;
+CREATE DATABASE `sidelc` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
+
+mariadb -u ext_dev -p43wkSQtCQYxFAc4d -h localhost -P 3306
+DROP SCHEMA IF EXISTS `extranet`;
+CREATE DATABASE `extranet` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
